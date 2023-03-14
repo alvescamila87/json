050846e0a5b9275957e8cae601ff67fa6dd9e066
Camila Alves <116912821+alvescamila87@users.noreply.github.com>
2023-03-14 20:53:36 -0300

    update aula de api com frontend com fetch

---
+++ rocketseat/API/index_api_front_fetch1.js
@@ -0,0 +1,11 @@
+// - API no Front-end com Fetch
+
+const express = require('express')
+
+const app = express()
+
+app.listen('3000')
+
+app.route('/').get( (req, res) => res.send( 'Olá'))
+app.route('/:variavel').get( (req, res) => res.send( req.params.variavel))
+app.route('/identidade/:nome').get( (req, res) => res.send( req.params.nome))
